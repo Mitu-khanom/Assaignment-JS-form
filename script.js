7ec537eddb4112ce383ebb0b96134e81295d8d37
Mitu-khanom <mitukhanom009@gmail.com>
2024-10-31 21:46:10 +0600

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const Form = document.querySelector("form");
 const Email = Form.querySelector("#email");
 const Password = Form.querySelector("#password");
+const Number = Form.querySelector("#number");
+const Date = Form.querySelector("#date");
+const Group = Form.querySelector("#group")
 // console.log(Password);
 
 
@@ -10,17 +13,27 @@ Form.addEventListener("submit", formHandler);
 
 function formHandler(e){
     e.preventDefault();
-    // console.log(e);
+    console.log(e);
 
-const formInfo = {
-    Email: Email.value,
-    Password: Password.value,
+    const formInfo = {
+        Email: Email.value,
+        Password: Password.value,
+        Number: Number.value,
+        Date: Date.value,
+        Group: Group.value,
+
+    }
+    Email.value = "";
+    Password.value = "";
+    Number.value = "";
+    Date.value = "";
+    Group.value = "";
+
+
+    
+    console.log(formInfo);
+
 
 }
-Email.value = "";
-Password.value = "";
-
-console.log(formInfo);
 
 
-}
